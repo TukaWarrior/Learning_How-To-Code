2237b85f29a76507adaf8e0787b888761f787ba7
Luca Bützberger <luca.buetzberger@gmail.com>
2023-11-14 17:53:15 +0100

    Codewars java exercises

---
+++ codewars/src/test/java/kyu7/ComplementaryDNATest.java
@@ -0,0 +1,24 @@
+package kyu7;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import com.example.kyu7.ComplementaryDNA;
+
+
+public class ComplementaryDNATest {
+    
+    @Test
+    public void test01() {
+       assertEquals("TTTT", ComplementaryDNA.makeComplement("AAAA"));
+    }
+
+    @Test
+    public void test02() {
+       assertEquals("TAACG", ComplementaryDNA.makeComplement("ATTGC"));
+    }
+
+    @Test
+    public void test03() {
+       assertEquals("CATA", ComplementaryDNA.makeComplement("GTAT"));
+    }  
+}
